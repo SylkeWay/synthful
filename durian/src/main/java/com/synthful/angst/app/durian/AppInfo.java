d3552958baf0853822f97d71ef4c5e40c874ac37
Sylke <Sylke@localhost>
2018-06-20 01:24:00 -0700

    ~

--- durian/src/main/java/com/synthful/angst/app/durian/AppInfo.java
+++ durian/src/main/java/com/synthful/angst/app/durian/AppInfo.java
@@ -11,33 +11,33 @@ import com.synthful.angst.model.Branch;
 import com.synthful.angst.model.Message;
 
 @Service
-public class AppInfo
-extends AAngsta{
-    
-	private Message message;
-	private String wName;
-	private int wValue;
-	
-	@Inject private Branch hampdenBranch;
-	@Inject private String greeting;
-	@Inject Address hampdenME;
-	
-	@Inject
-	public void setMessage (Message message) {
-		this.message = message;
-		logger.info("message={}", message);
-	}
-	
-	@Inject
-	public void setWally(Wally wally){
-		this.wName = wally.getName();
-		this.wValue = wally.getValue();
-		logger.info("wally={}", wally);		
-	}
-	
-	// Null injection values because injection is possible only after instantiation,
-	// which is when constructor completes
-	AppInfo(){
-	    logger.info("message:{} Location={} {}", message, hampdenBranch, hampdenME);
-	}
+public class AppInfo extends AAngsta {
+
+    private Message message;
+    private String wName;
+    private int wValue;
+
+    @Inject private Branch hampdenBranch;
+    @Inject private String greeting;
+    @Inject Address hampdenME;
+
+    @Inject
+    public void setMessage(Message message) {
+        this.message = message;
+        logger.info("message={}", message);
+    }
+
+    @Inject
+    public void setWally(Wally wally) {
+        this.wName = wally.getName();
+        this.wValue = wally.getValue();
+        logger.info("wally={}", wally);
+    }
+
+    // logger will print null injection values because
+    // injection is possible only after instantiation,
+    // which is when constructor completes
+    AppInfo() {
+        logger.info("message:{} Location={} {}", message, hampdenBranch, hampdenME);
+    }
 }

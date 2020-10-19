3439dc351c8c471b1c78c3c02ae751c2217890e5
SylkeWay <SylkeWay@synthful.com>
2020-10-19 02:12:46 -0700

    log test

--- durian/src/test/java/durian/ContextTest.java
+++ durian/src/test/java/durian/ContextTest.java
@@ -7,14 +7,17 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.synthful.angst.common.AAngsta;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:/spring/app-context.xml"})
-public class ContextTest {
+public class ContextTest implements AAngsta{
 
     @Inject
     private String greeting;
     @Test
     public void showEnv() {
+        getLogger().info("greeting:{}", greeting);
     }
 
 }
